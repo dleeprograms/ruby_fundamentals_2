8dca65c6ae51b3267de619591429b7402311e774
David Dongseung Lee <dleeprograms@outlook.com>
2016-01-13 17:29:43 -0500

    exercise_5

--- exercise_5.rb
+++ exercise_5.rb
@@ -1,7 +1,13 @@
-temperature_in_fahrenheit = f
 
-def f(num)
 
-(f - 32) * 5/9.to_i
+temperature_in_fahrenheit = 100
+
+def fahrenheit(num)
+
+celcious = (num - 32) * 5/9
+
+puts "fahrenheit temperature 100 is equal to #{celcious} in celcious."
 
 end
+
+fahrenheit(100)
