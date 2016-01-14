1617fb164df7575c44a9e49edffd6ad879271d83
David Dongseung Lee <dleeprograms@outlook.com>
2016-01-13 19:40:52 -0500

    exercise_6_progress_3

--- exercise_6.rb
+++ exercise_6.rb
@@ -3,5 +3,28 @@
 grocery_list = ["carrots", "toilet paper", "apples", "salmon"]
 
 
+grocery_list << "rice"
+
+#puts grocery_list
+
+  grocery_list.each do |item|
+
+    puts "*" + item
+
+  end
+
+puts grocery_list.length
+
+puts grocery_list.include?("banana")
+
+  if grocery_list.include?("banana") == false
+
+    puts "You don't need to pick up bananas today."
+
+  end
+
+puts grocery_list[1]
+
+puts grocery_list.delete ("salmon")
 
 puts grocery_list
