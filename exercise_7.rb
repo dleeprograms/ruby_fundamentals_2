a0363bab5369460fc103efb3676f1f1f86fe4658
David Dongseung Lee <dleeprograms@outlook.com>
2016-01-13 22:49:11 -0500

    exercise_7

--- exercise_7.rb
+++ exercise_7.rb
@@ -12,26 +12,32 @@ students.each do |k, v|
   puts "#{k}: #{v} student"
 
   end
+
+#   def keys(students)
+students[:cohort4] = 43
+#     # hash[:item2] = 2
+
+puts students
+#   end
 #
- students[:cohort4] = 43
- # hash[:item2] = 2
-
-
-
-
-# h = { "a" => 100, "b" => 200, "c" => 300, "d" => 400 }
-# h.keys   #=> ["a", "b", "c", "d"]
-#students.:cohort
+#
+# # h = { "a" => 100, "b" => 200, "c" => 300, "d" => 400 }
+# # h.keys   #=> ["a", "b", "c", "d"]
+# students.:cohort
+puts students.keys
 
 
 # my_hash.each do |key, value|
 #   my_hash[key] = "%#{value}%"
 # end
 
-students.each do |k, v|
-  students[:cohort] = "#{v}*0.05"
-end
+ students.each do |k, v|
+   v = v.to_i * (5.0/100) + v
+   puts "#{k} #{v}"
 
-# h = { "a" => 100, "b" => 200 }
-# h.delete("a")
 students.delete(:cohort2)
+
+ end
+#
+# # h = { "a" => 100, "b" => 200 }
+# # h.delete("a")
